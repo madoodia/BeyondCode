5d5cf054927c4162c45dec58eb6576eb1829de65
Reza Aarabi <madoodia@gmail.com>
2024-10-20 13:12:18 -0700

    Session02

--- main.cpp
+++ main.cpp
@@ -1,7 +1,10 @@
 #include <iostream>
+#include "stdio.h"
 
+// Entry point
 int main()
 {
 	std::cout << "Hello, World!" << std::endl;
+	printf("Hello, World!\n");
 	return 0;
 }
--- main.py
+++ main.py
@@ -4,4 +4,20 @@ import numpy
 # import PySide2
 import api
 
-print("Hello, World!")
+counter = 10
+
+def sayHello(msg):
+	global counter
+	counter -= 1
+	print("Your Message: ", msg)
+	if(counter > 0):
+		sayHello(msg)
+
+x = 5
+y = 10
+z = x + y
+
+sayHello(z)
+
+# fibonaaci series 1, 1, 2, 3, 5, 8, 13, 21, 34, 55
+# factorial 5! = 5 * 4 * 3 * 2 * 1 = 120
